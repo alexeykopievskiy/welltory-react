2578b08136ff9287437854faf0029c9eb491d02a
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 00:56:28 +0300

    create authReducer

--- src/js/reducers/authReducers.js
+++ src/js/reducers/authReducers.js
@@ -1,9 +1,45 @@
 let initialState = {
   token: null,
-  error: null
+  error: null,
+  fetching: false,
+  fetched: false
 }
 
 export default function reducer(state = initialState, action) {
+
+  switch (action.type) {
+
+    case "GET_TOKEN": {
+
+      return {
+        ...state,
+        fetching: true
+      }
+
+    }
+
+    case "GET_TOKEN_FULFILLED": {
+
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        token: action.payload
+      }
+
+    }
+
+    case "GET_TOKEN_REJECTED": {
+
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      }
+
+    }
+
+  }
 
   return state
 }
