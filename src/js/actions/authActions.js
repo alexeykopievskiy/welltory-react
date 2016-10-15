b850dc5f7dc7aff79b3554120887d369f2ff6ab8
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 01:18:15 +0300

    get token success

--- src/js/actions/authActions.js
+++ src/js/actions/authActions.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
+const EMAIL = 'smirnoffmg@gmail.com'
+const PASSWORD = 123456
+
 export function getToken() {
   return function(dispatch) {
     dispatch({type: 'GET_TOKEN'})
     axios({
       method: 'post',
-      url: 'https://stage-api.welltory.com/api2/api-token-auth/'
+      url: 'https://stage-api.welltory.com/api2/api-token-auth/',
+      data: {
+        email: EMAIL,
+        password: PASSWORD
+      }
     })
     .then(response => {
       dispatch({type: 'GET_TOKEN_FULFILLED', payload: response.data})
