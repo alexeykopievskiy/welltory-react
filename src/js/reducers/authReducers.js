ddf6ec33dbfa387db363df7ff35bb31fcb2dc424
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 00:49:36 +0300

    register reducer

---
+++ src/js/reducers/authReducers.js
@@ -0,0 +1,9 @@
+let initialState = {
+  token: null,
+  error: null
+}
+
+export default function reducer(state = initialState, action) {
+
+  return state
+}
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 
+import auth from './authReducers'
+
 export default combineReducers({
-  
+  auth
 })
