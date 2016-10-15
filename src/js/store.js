1fb60a9e731ce2f48ff58a89a52e05bf45ea7310
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 00:40:35 +0300

    creacte store for app

---
+++ src/js/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
+
+import reducer from './reducers'
+
+const middleware = applyMiddleware(logger(), promise(), thunk)
+
+// add reducer
+export default createStore(reducer, middleware)
