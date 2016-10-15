6401b5519694eba5bfdb674f63816aca96a10c3b
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 00:43:26 +0300

    register store

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import Layout from './containers/Layout'
 
 const app = document.getElementById('app')
 
-ReactDOM.render(<Layout />, app)
+ReactDom.render(
+<Provider store={store} >
+  <Layout />
+</Provider>, app)
