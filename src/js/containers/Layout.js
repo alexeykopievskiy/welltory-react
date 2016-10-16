66defae7c91e546dc29e4d89061d93ff2f320255
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-16 09:17:21 +0300

    add login form

--- src/js/containers/Layout.js
+++ src/js/containers/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Button, Form, FormGroup, Col, FormControl, ControlLabel } from 'react-bootstrap'
 
 import { getToken } from '../actions/authActions'
 
@@ -17,6 +18,37 @@ export default class Layout extends React.Component{
   }
 
   render() {
-    return <h1 className="test-n">Hello</h1>;
+
+    return <div>
+
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
+          </Col>
+          <Col sm={10}>
+            <FormControl type="email" placeholder="Email" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+            <FormControl type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button type="submit">
+              Sign in
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+      
+    </div>
   }
 }
