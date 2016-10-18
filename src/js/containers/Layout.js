40dd6fe4e2f57a2cac76ff836eef74fdf88560b1
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-18 19:24:06 +0300

    rebuilt main layout

--- src/js/containers/Layout.js
+++ src/js/containers/Layout.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
 import LoginPage from './login/LoginPage'
-
 import '../../assets/scss/test.scss'
 
 export default class Layout extends React.Component{
 
   constructor(props) {
     super(props);
-
   }
 
   render() {
 
-    return <div>
-
-      <LoginPage />
-
-    </div>
+    return(
+      <div className="layout layout--main">
+        <LoginPage />
+      </div>
+    )
   }
 }
