b05e7e270f5042f8830fc3d785b4ab4d5df83736
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-18 19:21:32 +0300

    remove logs

--- src/js/containers/login/LoginPage.js
+++ src/js/containers/login/LoginPage.js
@@ -5,7 +5,6 @@ import { getToken } from '../../actions/authActions'
 import LoginForm from './LoginForm'
 
 @connect(store => {
-  console.log(store.auth, 'store');
   return {
     token: store.auth.token
   }
@@ -17,12 +16,10 @@ export default class LoginPage extends React.Component{
   }
 
   handleSubmit = values => {
-
     this.props.dispatch(getToken(values))
   }
 
   render() {
-    console.log(this.props, 'pro');
     return <LoginForm onSubmit={this.handleSubmit} />
   }
 }
